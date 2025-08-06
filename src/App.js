039e2a45f2b263b893c32d62ba2afe9930e9f542
carolayuss <carolayuss@gmail.com>
2025-08-06 02:14:14 -0500

    recuperando parte del codigo

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import { Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar/Navbar';
 import Heart from './components/Heart/Heart';
 import Login from './components/Login/Login';
-import RegistroCliente from './components/RegistroCliente/RegistroCliente';
+
 import Footer from './components/Footer/Footer';
-import { CarritoProvider } from './components/Carrito/CarritoContext';
-import CarritoPage from './components/Carrito/CarritoPage';
+
 import './App.css'
 
 
 function App() {
   return (
     <>
-    <CarritoProvider>
+    
       <Navbar />
       <Routes>
         <Route path="/" element={<><Heart /><Footer /></>} /> 
         <Route path="/login" element={<Login />} />
-        <Route path="/registrate" element={<RegistroCliente />} />
-        <Route path="/carrito" element={<CarritoPage />} />
+        
         
       </Routes>
-      </CarritoProvider>
+      
     </>
   );
 }
